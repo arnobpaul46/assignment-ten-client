ee528b9c15595e059d18f399571927069d629565
arnobpaul46 <arnobpaulbd@gmail.com>
2026-06-25 13:37:39 +0600

    login wrap suspense

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,14 +1,12 @@
+import { Suspense } from 'react';
 import LoginForm from "@/components/auth/LoginForm";
-
-
-
-
 
 export default function SignInPage() {
   return (
     <div className="min-h-screen bg-[#09090b]">
-      
-      <LoginForm/>
+      <Suspense fallback={<div className="text-white text-center mt-20">Loading...</div>}>
+        <LoginForm />
+      </Suspense>
     </div>
   );
 }
